b12629e3ea3be541ff25a4d818c17379657ebbe3
Quang Nguyen <quangn2595@gmail.com>
2019-06-18 08:43:44 -0700

    Adding table as result and user stories

--- js/app.js
+++ js/app.js
@@ -16,9 +16,12 @@
 
 var filepaths = ['/img/bag.jpg', '/img/banana.jpg', '/img/bathroom.jpg', '/img/boots.jpg', '/img/breakfast.jpg', '/img/bubblegum.jpg', '/img/chair.jpg', '/img/cthulhu.jpg', '/img/dog-duck.jpg', '/img/dragon.jpg', '/img/pen.jpg', '/img/pet-sweep.jpg', '/img/scissors.jpg', '/img/shark.jpg', '/img/sweep.png', '/img/tauntaun.jpg', '/img/unicorn.jpg', '/img/usb.gif', '/img/water-can.jpg', '/img/wine-glass.jpg'];
 var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 var imgSection = document.getElementById('image');
+var table
 var numberOfImg = 3;
 var totalClick = 0;
+var maxClick = 5;
 var indexArray = []; 
 var eventFired = false;
 var imgSrc;
@@ -83,8 +86,15 @@ function displayImages() {
 
 function eventListener() { 
   var eachImage = document.getElementsByClassName('images');
-  for(var i = 0; i < eachImage.length; i++) { 
-    eachImage[i].addEventListener('click', eventClick);
+  if (totalClick < maxClick) { 
+    for(var i = 0; i < eachImage.length; i++) { 
+      eachImage[i].addEventListener('click', eventClick);
+    }
+  } else { 
+    tHeader();
+    for(var j = 0; j < Products.list.length; j++) { 
+      tBody(j);
+    }
   }
 }
 
@@ -93,11 +103,59 @@ function checkIndex(element) {
 }
 
 function eventClick(e) { 
+  totalClick++;
   imgSrc = e.target.src;
   Products.list[filepaths.findIndex(checkIndex)].numClick += 1;
   removePreviousImg();
   displayImages();
-  totalClick++;
+}
+
+function addTableElement(element, content) { 
+  var cell = document.createElement{'tr'};
+}
+
+function tHeader() { 
+  var tableHeader = document.getElementById('table-header');
+  var tableRow = document.createElement('tr');
+  var cell = document.createElement('th');
+  cell.textContent = 'Name of Product';
+  tableRow.appendChild(cell);
+  
+  cell = document.createElement('td');
+  cell.textContent = 'Number of times products shown';
+  tableRow.appendChild(cell);
+
+  cell = document.createElement('td');
+  cell.textContent = 'Number of times products clicked';
+  tableRow.appendChild(cell);
+
+  cell = document.createElement('td');
+  cell.textContent = 'Percentage of times product is clicked when it is shown';
+  tableRow.appendChild(cell);
+
+  tableHeader.appendChild(tableRow);
+}
+
+function tBody(index) { 
+  var tableBody = document.getElementById('table-body');
+  var tableRow = document.createElement('tr');
+  var cell = document.createElement('td');
+  cell.textContent = Products.list[index].name;
+  tableRow.appendChild(cell);
+
+  cell = document.createElement('td');
+  cell.textContent = Products.list[index].numShown;
+  tableRow.appendChild(cell);
+
+  cell = document.createElement('td');
+  cell.textContent = Products.list[index].numClick;
+  tableRow.appendChild(cell);
+
+  cell = document.createElement('td');
+  cell.textContent = Products.list[index].numClick / Products.list[index].numShown;
+  tableRow.appendChild(cell);
+
+  tableBody.appendChild(tableRow);
 }
 
 for (var i = 0; i < filepaths.length; i++) { 
@@ -105,40 +163,3 @@ for (var i = 0; i < filepaths.length; i++) {
 }
  
 displayImages();
-
-// function getRandomImages(randomImageIndex, imageSrc) { 
-//   for (var i = 0; i < numberOfImg; i++) {
-//     randomImageIndex = Math.floor(Math.random() * images.length);
-//     imageSrc = images[randomImageIndex];
-//     var img = document.createElement("img");
-//     img.className = 'images';
-//     img.src = imageSrc;
-//     imgSection.appendChild(img);
-//   }
-// }
-
-// function displayRandomImages() { 
-//   var randomImageIndex;
-//   var imageSrc;
-
-//   numClick++;
-//   if (numClick === 0) { 
-//     getRandomImages(randomImageIndex, imageSrc);
-//   } else { 
-//     getRandomImages(randomImageIndex, imageSrc);
-//     removePreviousImages(imgSection);
-//   }
-
-//   eachImage = document.getElementsByClassName('images');
-// }
-
-// displayRandomImages();
-// var eachImage = document.getElementsByClassName('images');
-
-// // for (var i = 0; i < eachImage.length; i++) {
-//   while (numClick)
-
-//   eachImage[0].addEventListener('click', displayRandomImages);
-//   eachImage[1].addEventListener('click', displayRandomImages);
-//   eachImage[2].addEventListener('click', displayRandomImages);
-// // }
--- user_stories.md
+++ user_stories.md
@@ -1,1 +1,9 @@
-As a developer 
+As a developer, I want to randomize the products shown on screen to avoid any bias toward certain products.
+
+As a marketer, I want to know the percentage of times a product was clicked so that the data can be used for the catalog.
+
+As a developer, I want a clean looking design of the website so that the user can enjoy the experience of taking the survey. 
+
+As a user, I dont want an image to repeat itself right after it was shown on the screen so that it is less annoying. 
+
+As a marketer, I want some sort of graphical representation of the data collected from the survey so that the catalog can be better put together. 
